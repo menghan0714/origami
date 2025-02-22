64d3c89305cc907652b9829b6609b18be019f5f9
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-02-22 18:27:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
